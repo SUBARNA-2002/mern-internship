1a2fd889590f6e52f9d5cf44b88e48af17238bf3
Subarna Keshari Sutar <85243577+SUBARNA-2002@users.noreply.github.com>
2023-05-30 22:53:47 +0530

    Add files via upload

---
+++ 30-05-23/index.js
@@ -0,0 +1,42 @@
+// const button=document.getElementById("btn");
+const button1=document.getElementById("btn1");
+const button2=document.getElementById("btn2");
+const button3=document.getElementById("btn3");
+
+const textarea=document.getElementById("content")
+button1.addEventListener("click",()=>{
+    // // console.log("clicked")
+    // const div=document.getElementById("show");
+    // const image = document.createElement("img");
+    // image.src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+    // div.append(image);
+
+    textarea.innerText="This is my Text";
+})
+button2.addEventListener("click",()=>{
+    textarea.innerText="";
+})
+// const table=document.querySelector("table");
+// button1.addEventListener("click",()=>{
+//     const tr=document.createElement("tr");
+//     const td1=document.createElement("td");
+//     td1.innerText="1";
+//     const td2=document.createElement("td");
+//     td2.innerText="Subarna";
+//      td2.style.color="red";
+//     const td3=document.createElement("td");
+//     td3.innerText="Singh";
+//     tr.append(td1,td2,td3);
+//     table.append(tr)
+// })
+// button2.addEventListener("click",()=>{
+//   window.location.reload();
+// })
+// button3.addEventListener("click",()=>{
+//     const div = document.getElementById("show")
+//     const p1=document.createElement("p");
+//     p1.innerText="yjrhdnrreytunrdy"
+//     div.append(p1);
+   
+//   })
+
